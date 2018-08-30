077fcf162f678a5fe6c0f073c7256e16244a1b64
loisovervoorde <17615620+loisovervoorde@users.noreply.github.com>
2018-08-31 00:59:17 +0100

    Percents sorted

--- Yields.py
+++ Yields.py
@@ -6,7 +6,7 @@
 import re # for regex
 
 # files
-fnamesi = "Tex\Results_and_discussion_intro_2.tex Tex\Experimental_AHL_analogue_conjugates.tex Tex\Results_and_discussion_nMeOA.tex Tex\Experimental_general.tex Tex\Results_and_discussion_PA_AIs.tex Tex\Experimental_triazoles.tex Tex\Results_and_discussion_SHLs.tex Tex\Future_work.tex Tex\Results_and_discussion_triazoles.tex Tex\Introduction.tex Tex\Summary.tex Tex\NMRs.tex Tex\Test.tex Tex\Nomenclature.tex Tex\Thesis.tex Tex\Results_and_discussion_antibiotics.tex Tex\Thesis_working_chapter.tex Tex\Results_and_discussion_bio_1.tex Tex\TitlePage.tex Tex\Results_and_discussion_bio_2.tex"
+fnamesi = "Tex/Abstract.tex Tex/Results_and_discussion_cleavable.tex Tex/Acknowledgements.tex Tex/Results_and_discussion_HOcy5.tex Tex/CompoundList.tex Tex/Results_and_discussion_HOcy6.tex Tex/Conclusions.tex Tex/Results_and_discussion_intro_1.tex Tex/Experimental_1st_year.tex Tex/Results_and_discussion_intro_2.tex Tex/Experimental_AHL_analogue_conjugates.tex Tex/Results_and_discussion_nMeOA.tex Tex/Experimental_general.tex Tex/Results_and_discussion_PA_AIs.tex Tex/Experimental_triazoles.tex Tex/Results_and_discussion_SHLs.tex Tex/Future_work.tex Tex/Results_and_discussion_triazoles.tex Tex/Introduction.tex Tex/Summary.tex Tex/NMRs.tex Tex/Test.tex Tex/Nomenclature.tex Tex/Thesis.tex Tex/Results_and_discussion_antibiotics.tex Tex/Thesis_working_chapter.tex Tex/Results_and_discussion_bio_1.tex Tex/TitlePage.tex Tex/Results_and_discussion_bio_2.tex"
 
 for fnamei in fnamesi.split():
 
@@ -19,7 +19,7 @@ for fnamei in fnamesi.split():
     text = fi.read() 
 
     # regexing
-    text = re.sub(' ([0-9]+) \\\%', ' \g<1>\%', text)
+    text = re.sub('([^\.][0-9]+) \\\%', '\g<1>\%', text)
     text = re.sub('([0-9]+)\.([0-4])([0-9]*) \\\%', '\g<1>\%', text)
     for match in re.finditer('([0-9]+)\.([5-9])([0-9]*) \\\%', text):
           text = text.replace(match.group(0),str(int(match.group(1))+1)+'\%',1)
