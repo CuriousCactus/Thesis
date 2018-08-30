d3cf5b3a40145dcf6d6d3c9c6896141adf0a9965
loisovervoorde <17615620+loisovervoorde@users.noreply.github.com>
2018-08-31 00:25:35 +0100

    before % change

--- Yields.py
+++ Yields.py
@@ -1,8 +1,12 @@
-# python program to round percentages to nearest integers
+# python program to round percentages to nearest integers and remove spaces
+# written to be applied to tex files, with percentages in the format:
+#             "13.7 \%" -> "14\%"
+
+
 import re # for regex
 
 # files
-fnamesi = "Results_and_discussion_antibiotics.tex"
+fnamesi = "Tex\Results_and_discussion_intro_2.tex Tex\Experimental_AHL_analogue_conjugates.tex Tex\Results_and_discussion_nMeOA.tex Tex\Experimental_general.tex Tex\Results_and_discussion_PA_AIs.tex Tex\Experimental_triazoles.tex Tex\Results_and_discussion_SHLs.tex Tex\Future_work.tex Tex\Results_and_discussion_triazoles.tex Tex\Introduction.tex Tex\Summary.tex Tex\NMRs.tex Tex\Test.tex Tex\Nomenclature.tex Tex\Thesis.tex Tex\Results_and_discussion_antibiotics.tex Tex\Thesis_working_chapter.tex Tex\Results_and_discussion_bio_1.tex Tex\TitlePage.tex Tex\Results_and_discussion_bio_2.tex"
 
 for fnamei in fnamesi.split():
 
@@ -10,21 +14,22 @@ for fnamei in fnamesi.split():
 
     # opening input file
     fi = open(fnamei, 'r')
-   
+    
     # reading
-    text = fi.read()
+    text = fi.read() 
 
     # regexing
-    text = re.sub('([0-9]+)\.([0-4])([0-9]*)\%', '\g<1>%', text)
-    for match in re.finditer('([0-9]+)\.([5-9])([0-9]*)\%', text):
-          text = text.replace(match.group(0),str(int(match.group(1))+1)+'%',1)
-   
-    # closing input file     
-    fi.close()
-   
+    text = re.sub(' ([0-9]+) \\\%', ' \g<1>\%', text)
+    text = re.sub('([0-9]+)\.([0-4])([0-9]*) \\\%', '\g<1>\%', text)
+    for match in re.finditer('([0-9]+)\.([5-9])([0-9]*) \\\%', text):
+          text = text.replace(match.group(0),str(int(match.group(1))+1)+'\%',1)
+    
+    # closing input file      
+    fi.close() 
+    
     # output file name
-    fnameo = "rounded_" + fnamei
-   
+    fnameo =  fnamei
+    
     print ("output to", fnameo)
 
     # writing to output file
